c32598fa9d2270c7f536e8d5b7eb005b4677138e
Simon Jockers <hello@simonjockers.com>
2016-04-23 15:45:54 +0200

    Integrate wit.ai middleware for simple NLP.

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 'use strict'
 
+// Load environmental constiables from .env file:
 require('dotenv').config()
-
 const SLACK_TOKEN = process.env.SLACK_TOKEN
+const GIPHY_TOKEN = process.env.GIPHY_TOKEN
+const WITAI_TOKEN = process.env.WITAI_TOKEN
 
-if (!SLACK_TOKEN) {
-  console.log('Error: Specify token in environment')
-  process.exit(1)
+if (!SLACK_TOKEN || !GIPHY_TOKEN || !WITAI_TOKEN) {
+  throw new Error('Specify API tokens in environment!')
 }
 
-const Botkit = require('botkit')
+// Imports:
+const botkit = require('botkit')
+const wit = require('botkit-middleware-witai')({token: WITAI_TOKEN})
+const giphy = require('giphy-api')(GIPHY_TOKEN)
 
-const controller = Botkit.slackbot({
+// Initialize and spawn a slackbot using Botkit:
+const controller = botkit.slackbot({
   debug: false,
   storage: undefined
 })
@@ -22,8 +27,26 @@ controller.spawn({token: SLACK_TOKEN}).startRTM((error) => {
   }
 })
 
-controller.hears(['gif of a (.*)'], ['direct_message', 'direct_mention'], (bot, message) => {
-  const queryString = message.match[1]
+// Initialize wit.ai middleware:
+controller.middleware.receive.use(wit.receive)
 
-  bot.reply(message, `You wanted me to find this GIF of a ${queryString}`)
+// Make the bot listen to direct messages and direct mentions, using wit.ai middleware:
+controller.hears('default_intent', ['direct_message', 'direct_mention'], wit.hears, (bot, message) => {
+  // Get entities from the intents detected by wit.ai:
+  const entities = message.intents[0].entities
+
+  // If wit.ai was able to extract entities, get going:
+  if (entities.has_gif_request && entities.search_query) {
+    const queryString = entities.search_query[0].value
+
+    // Search for a GIF using the Giphy API:
+    giphy.search(queryString).then((result) => {
+      // Return the first search result:
+      const gif = result.data.pop()
+      bot.reply(message, `there you go: ${queryString}`)
+      bot.reply(message, gif.images.fixed_width.url)
+    })
+  } else {
+    bot.reply(message, 'Sorry, I did not get that. Could you try it again?')
+  }
 })
